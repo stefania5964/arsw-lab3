408bcf4bf5c662811e70bf59e9130a3194999c58
unknown <tefila123@hotmail.com>
2023-02-11 17:40:08 -0500

    primera parte completa

--- src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -6,6 +6,8 @@
 package edu.eci.arst.concprg.prodcons;
 
 import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -22,13 +24,18 @@ public class Consumer extends Thread{
     
     @Override
     public void run() {
-        while (true) {
+
 
             if (queue.size() > 0) {
-                int elem=queue.poll();
-                System.out.println("Consumer consumes "+elem);                                
+                int elem = queue.poll();
+                System.out.println("Consumer consumes " + elem);
             }
-            
-        }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+
+                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
     }
 }
